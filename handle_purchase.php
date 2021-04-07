69f2502e611e9919a85cd306106efb380c864306
oreniv <oreniv@mta.ac.il>
2021-04-07 16:49:40 +0300

    sql insertion validation added !

--- handle_purchase.php
+++ handle_purchase.php
@@ -5,6 +5,7 @@ session_start();
 if($_SESSION['userID'] == -1)
 {
 // NOTICE THE QUOTES AT THE EDGES OF THE STRINGS  
+
 $productID = (int)$_SESSION['productID'];
 $quantity = (int)$_POST['quantity'] ; 
 $credit_card = (int)$_POST['credit_card'];
@@ -15,6 +16,9 @@ $buyer_email = "'".$_POST['buyer_email']."'";
 $sqlPurchaseData = "CALL insert_transaction_history_anonymous($productID,$quantity,$credit_card,$full_name,$shipping_address,$buyer_email)";
 if (!mysqli_query($conn,$sqlPurchaseData)) {
     echo("Error description: " . mysqli_error($conn));
+    $_SESSION['db_error'] = True;
   }
+else 
+$_SESSION['db_error'] = False;
   mysqli_close($conn);
 }
